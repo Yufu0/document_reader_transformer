f6fa33fb945c48a54f8856f6cee4c50a3b14d165
Celio <buericelio@cy-tech.fr>
2023-11-17 09:59:47 +0100

    test donut

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
 from transformers import VisionEncoderDecoderConfig, AutoConfig, VisionEncoderDecoderModel, AutoModel, RobertaTokenizer, \
-    TFBertTokenizer, PreTrainedModel, AutoTokenizer
+    TFBertTokenizer, PreTrainedModel, AutoTokenizer, DonutProcessor
 from MyDocParser.DocParser import DocParserConfig, DocParserModel
-import numpy as np
+
 import torch
 
 if __name__ == '__main__':
-    AutoConfig.register("docparser", DocParserConfig)
-    AutoModel.register(DocParserConfig, DocParserModel)
+    # AutoConfig.register("docparser", DocParserConfig)
+    # AutoModel.register(DocParserConfig, DocParserModel)
+    #
+    # config = VisionEncoderDecoderConfig.from_pretrained("./MyDocParser/")  # fichier config.json
+    #
+    # model = VisionEncoderDecoderModel(config=config)
+    #
+    # inputs = torch.ones(1, 3, 100, 100)
+    # inputs_ids = torch.ones(10, 512).int()
+    #
+    # outputs = model(
+    #     pixel_values=inputs,
+    #     decoder_input_ids=inputs_ids
+    # )
+    #
+    # tokenizer = AutoTokenizer.from_pretrained("roberta-base")
 
-    config = VisionEncoderDecoderConfig.from_pretrained("./MyDocParser/")  # fichier config.json
+    processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip")
+    model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip")
 
-    model = VisionEncoderDecoderModel(config=config)
-
-    inputs = torch.ones(1, 3, 100, 100)
+    image = torch.ones(3, 500, 500)
+    pixel_values = processor(image, return_tensors="pt").pixel_values
     inputs_ids = torch.ones(10, 512).int()
 
     outputs = model(
-        pixel_values=inputs,
-        decoder_input_ids=inputs_ids
+        pixel_values=pixel_values,
     )
 
-    tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-
-    #
-    # last_hidden_states = outputs.last_hidden_state
-    # print(last_hidden_states.shape, last_hidden_states)
-    # print(tokenizer.decode(tokenizer.encode("Hello, my dog is cute")))
-    # print(len(tokenizer.get_vocab()))
-
-
-    # model = RobertaModel.from_pretrained("roberta-base")
 
 
