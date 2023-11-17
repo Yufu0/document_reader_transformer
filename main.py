27fe7d89665bfee62e2bc6e364a5eda7349d5ba6
Celio <buericelio@cy-tech.fr>
2023-11-17 11:31:23 +0100

    update model

--- main.py
+++ main.py
@@ -1,37 +1,40 @@
 from transformers import VisionEncoderDecoderConfig, AutoConfig, VisionEncoderDecoderModel, AutoModel, RobertaTokenizer, \
     TFBertTokenizer, PreTrainedModel, AutoTokenizer, DonutProcessor
-from MyDocParser.DocParser import DocParserConfig, DocParserModel
+from MyDocParser.DocParser import DocParserModel, DocParserConfig
+
 
 import torch
 
 if __name__ == '__main__':
-    # AutoConfig.register("docparser", DocParserConfig)
-    # AutoModel.register(DocParserConfig, DocParserModel)
+    AutoConfig.register("docparser", DocParserConfig)
+    AutoModel.register(DocParserConfig, DocParserModel)
+
+    config = VisionEncoderDecoderConfig.from_pretrained("./MyDocParser/")  # fichier config.json
+
+    model = VisionEncoderDecoderModel(config=config)
+
+    inputs = torch.ones(1, 3, 100, 100)
+    input_ids = torch.ones(10, 512).int()
+    outputs = model(
+        pixel_values=inputs,
+        decoder_input_ids=input_ids
+    )
     #
-    # config = VisionEncoderDecoderConfig.from_pretrained("./MyDocParser/")  # fichier config.json
+    # tokenizer = AutoTokenizer.from_pretrained("roberta-base")
     #
-    # model = VisionEncoderDecoderModel(config=config)
+    # processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip")
+    # model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip")
     #
-    # inputs = torch.ones(1, 3, 100, 100)
+    # image = torch.ones(1,3, 500, 500)
+    # pixel_values = processor(image, return_tensors="pt").pixel_values
     # inputs_ids = torch.ones(10, 512).int()
     #
     # outputs = model(
-    #     pixel_values=inputs,
-    #     decoder_input_ids=inputs_ids
+    #     pixel_values=pixel_values,
     # )
-    #
-    # tokenizer = AutoTokenizer.from_pretrained("roberta-base")
-
-    processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip")
-    model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip")
-
-    image = torch.ones(3, 500, 500)
-    pixel_values = processor(image, return_tensors="pt").pixel_values
-    inputs_ids = torch.ones(10, 512).int()
-
-    outputs = model(
-        pixel_values=pixel_values,
-    )
-
+    # print(config.encoder.ci)
+    # model = DocParserModel(config.encoder)
+    # output = model(pixel_values=image)
+    # print(output[0].shape)
 
 
