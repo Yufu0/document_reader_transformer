4a1493d7b4f66766ab4b4a9a9f8c5ef854b99f80
Celio <buericelio@cy-tech.fr>
2023-11-21 11:02:39 +0100

    training"

--- training.py
+++ training.py
@@ -17,38 +17,32 @@ def load_tokenizer():
     return tokenizer
 
 
-def preprocessing(image_ocr, tokenizer):
+def preprocessing(image_ocr, tokenizer=None, max_length=512):
+    if tokenizer is None:
+        tokenizer = load_tokenizer()
+
+    ocr_tokens = tokenizer(
+        " ".join([f"<{elem['label']} > {elem['text']} <{elem['label']} />" for elem in image_ocr["ocr"]])
+    )["input_ids"]
+
+    if len(ocr_tokens) > max_length:
+        ocr_tokens = ocr_tokens[:max_length]
+    if len(ocr_tokens) < max_length:
+        ocr_tokens = ocr_tokens + [tokenizer.pad_token_id] * (max_length - len(ocr_tokens))
+
     return {
-        "image": image_ocr["image"],
-        "ocr": tokenizer(
-            " ".join([f"<{elem['label']} > {elem['text']} <{elem['label']} />" for elem in image_ocr["ocr"]])
-        )["input_ids"]
+        "ocr": ocr_tokens
     }
 
-def preprocessing_generator(image_ocr, tokenizer):
-    return {
-        "image": image_ocr["image"],
-        "ocr": tokenizer(
-            " ".join([f"<{elem['label']} > {elem['text']} <{elem['label']} />" for elem in image_ocr["ocr"]])
-        )["input_ids"]
-    }
 
 def train(epochs, model, tokenizer, train_loader, optimizer):
+    input_ids = torch.ones(1, 512).long().to(device)
     for epoch in range(epochs):
         losses = []
         for batch in train_loader:
-            pixel_values = batch["image"]
-            pixel_values = pixel_values.permute(0, 3, 1, 2)
-            pixel_values = pixel_values.float()
-            labels = preprocessing(batch["ocr"], tokenizer)
+            pixel_values, labels = batch["image"], batch["ocr"]
+            pixel_values = pixel_values.permute(0, 3, 1, 2).float()
 
-            if len(labels) > 512:
-                labels = labels[:300]
-            length = len(labels)
-            print(device)
-            labels = torch.Tensor(labels, device=device).long()
-
-            input_ids = torch.ones(1, length).long().to(device)
             output = model(pixel_values=pixel_values, decoder_input_ids=input_ids, labels=labels)
 
             loss = output.loss
@@ -59,11 +53,14 @@ def train(epochs, model, tokenizer, train_loader, optimizer):
             loss.backward()
             optimizer.step()
             optimizer.zero_grad()
+
+            del output
+            del loss
             
             gc.collect()
         
-        if epoch % 5 == 0:
-            save_model(model)
+        # if epoch % 5 == 0:
+        #     save_model(model)
         print(f"epoch {epoch} : {sum(losses) / len(losses)}")
 
 def evaluate (model, img_test):
@@ -73,30 +70,24 @@ def evaluate (model, img_test):
 
 if __name__ == '__main__':
     # load the model
-    # model = load_model().to(device)
+    model = load_model().to(device)
 
     # load tokenizer
     tokenizer = load_tokenizer()
 
     # load the dataset
     dataset = load_dataset("arvindrajan92/sroie_document_understanding", split="train")
-    preprocessing_ocr = partial(preprocessing, tokenizer=tokenizer)
-    dataset.map(preprocessing_ocr)
-    print(dataset[0])
-    #
-    # data.set_format("torch", columns=["image", "ocr"])
-    # train_loader = DataLoader(dataset=data, batch_size=1, shuffle=True)
-    #
-    #
-    # print(device)
-    #
-    #
-    # model.config.decoder_start_token_id = tokenizer.cls_token_id
-    # model.config.pad_token_id = tokenizer.pad_token_id
-    #
-    # # optimizer for nlp
-    # optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
-    #
-    #
-    # train(1, model, tokenizer, train_loader, optimizer)
+    dataset = dataset.map(partial(preprocessing, tokenizer=tokenizer))
+    dataset.set_format("torch", columns=["image", "ocr"])
+
+    train_loader = DataLoader(dataset=dataset, batch_size=1, shuffle=True)
+
+    model.config.decoder_start_token_id = tokenizer.cls_token_id
+    model.config.pad_token_id = tokenizer.pad_token_id
+
+    # optimizer for nlp
+    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
+
+
+    train(1, model, tokenizer, train_loader, optimizer)
 
