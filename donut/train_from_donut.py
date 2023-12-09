410163894f2ffea7494d04e609bee47bf35900c5
Bueri <bueri@chalmers.se>
2023-12-09 20:12:29 +0100

    a

--- donut/train_from_donut.py
+++ donut/train_from_donut.py
@@ -1,8 +1,10 @@
+from typing import re
+
 from datasets import load_dataset
 from torch.utils.data import DataLoader, IterableDataset
 import torch
 from tqdm import tqdm
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, VisionEncoderDecoderModel, DonutProcessor
 import gc
 import numpy as np
 
@@ -56,14 +58,14 @@ def preprocessing(dataset, tokenizer=None, max_length=512):
         ocr_tokens = torch.tensor(ocr_tokens)
 
         image = line["image"]
-        if image.width * image.height > 1_000_000:
-            ratio = np.sqrt(1_000_000 / (image.width * image.height))
-            image = image.resize((int(image.width * ratio), int(image.height * ratio)))
+        # if image.width * image.height > 1_000_000:
+        #     ratio = np.sqrt(1_000_000 / (image.width * image.height))
+        #     image = image.resize((int(image.width * ratio), int(image.height * ratio)))
         img = np.array(image)
         img = img.transpose((2, 0, 1))
         img = img.astype(np.float32)
-        img = img / 255.
-        img = torch.tensor(img)
+        # img = img / 255.
+        # img = torch.tensor(img)
 
         yield (img, ocr_tokens)
 
@@ -105,7 +107,9 @@ def train(epochs, model, tokenizer, training_dataloader, optimizer, scheduler, a
 
 
 def evaluate(model, pixel_values, labels, tokenizer):
+
     with torch.no_grad():
+        decoder_input_ids = torch.ones(1, 512).int()
         output = model(pixel_values=pixel_values, labels=labels)
         output = output.logits.detach().cpu()
         output = output.argmax(dim=-1)
@@ -115,26 +119,51 @@ def evaluate(model, pixel_values, labels, tokenizer):
 
 def main():
     # load tokenizer
-    tokenizer = load_tokenizer()
+    # tokenizer = load_tokenizer()
 
     # load the dataset
-    training_dataloader = load_dataset_sroie(tokenizer=tokenizer)
+    # training_dataloader = load_dataset_sroie(tokenizer=tokenizer)
 
     # load the model
-    model = load_model().to(device)
-    model.train()
+    model = VisionEncoderDecoderModel.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip")
 
-    model.config.decoder_start_token_id = tokenizer.cls_token_id
-    model.config.pad_token_id = tokenizer.pad_token_id
+    processor = DonutProcessor.from_pretrained("naver-clova-ix/donut-base-finetuned-rvlcdip", use_fast=False)
+    # model.train()
+#AutoTokenizer
+    # model.config.decoder_start_token_id = tokenizer.cls_token_id
+    # model.config.pad_token_id = tokenizer.pad_token_id
 
-    # optimizer for nlp
-    optimizer = torch.optim.AdamW(model.parameters(), lr=1e-5)
-    scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.9)
+    dataset = load_dataset("hf-internal-testing/example-documents", split="test")
+    image = dataset[1]["image"]
 
-    accelerator = accelerate.Accelerator()
-    model, optimizer, training_dataloader, scheduler = accelerator.prepare(
-        model, optimizer, training_dataloader, scheduler
+    # prepare decoder inputs
+    task_prompt = "<s_rvlcdip>"
+    decoder_input_ids = processor.tokenizer(task_prompt, add_special_tokens=False, return_tensors="pt").input_ids
+
+    pixel_values = processor(image, return_tensors="pt").pixel_values
+    print(pixel_values.shape)
+
+    outputs = model.generate(
+        pixel_values.to(device),
+        decoder_input_ids=decoder_input_ids.to(device),
+        max_length=model.decoder.config.max_position_embeddings,
+        pad_token_id=processor.tokenizer.pad_token_id,
+        eos_token_id=processor.tokenizer.eos_token_id,
+        use_cache=True,
+        bad_words_ids=[[processor.tokenizer.unk_token_id]],
+        return_dict_in_generate=True,
     )
+
+    sequence = processor.batch_decode(outputs.sequences)[0]
+    sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(processor.tokenizer.pad_token, "")
+    sequence = re.sub(r"<.*?>", "", sequence, count=1).strip()  # remove first task start token
+    print(processor.token2json(sequence))
+
+
+    # accelerator = accelerate.Accelerator()
+    # model, optimizer, training_dataloader, scheduler = accelerator.prepare(
+    #     model, optimizer, training_dataloader, scheduler
+    # )
     n = 1001
     # train(n, model, tokenizer, training_dataloader, optimizer, scheduler, accelerator)
 
